3940bd7131b7d410ecee7049353fd2baf150114d
Øyvind Moldestad <moldestad@gmail.com>
2025-02-17 21:15:53 +0100

    fix: Rename App to AppWrapper

--- ui/src/AppWrapper.tsx
+++ ui/src/AppWrapper.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-function App() {
+function AppWrapper() {
   return <RouterProvider router={router} />
 }
 
-export default App
+export default AppWrapper
